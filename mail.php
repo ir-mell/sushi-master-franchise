be82fe4b842e5c48b55bdf0b5632ced09aca6f49
Igor Piskunov <unuservize@gmail.com>
2018-01-26 20:01:51 +0300

    Added mail php script

--- mail.php
+++ mail.php
@@ -1,17 +1,30 @@
 <?php
 
-$to = "piskunovigorm@gmail.com";
-$subject = "Sushi Master Franchise";
-$txt = "Hello world!";
+header("Access-Control-Allow-Origin: *");
 
-$headers = array(
-  'From: "Sushi Master Franchise" <support@sushi-master-franchise.com>' ,
-  'Reply-To: "Sushi Master Franchise" <support@sushi-master-franchise.com>' ,
-  'X-Mailer: PHP/' . phpversion() ,
-  'MIME-Version: 1.0' ,
-  'Content-type: text/html; charset=iso-8859-1'
-);
-$headers = implode( "\r\n" , $headers );
+try{
+	$to = "piskunovigorm@gmail.com";
+	$subject = $_POST['clientName']." - звонок Sushi Master Franchise";
 
-mail($to,$subject,$txt,$headers);
+	$txt = "Клиент: ".$_POST['clientName']."\r\n".
+		   "Город: ".$_POST['clientCity']."\r\n".
+		   "Телефон: ".$_POST['clientPhone'];
+
+	$headers = array(
+	  'From: "Sushi Master Franchise" <support@sushi-master-franchise.com>' ,
+	  'Reply-To: "Sushi Master Franchise" <support@sushi-master-franchise.com>' ,
+	  'X-Mailer: PHP/' . phpversion() ,
+	  'MIME-Version: 1.0' ,
+	  'Content-type: text/html; charset=iso-8859-1'
+	);
+	$headers = implode( "\r\n" , $headers );
+
+	mail($to,$subject,$txt,$headers);
+
+	echo '{ "status": 1 }';
+
+} catch(Excetion $e){
+	echo '{ "status": 0 }';
+}
+
 ?>
